f42b8587dc1f33cf98d624327b74aa9e8680992e
Hauzan Tsaaqif Mushaddaq <hauzantsaaqif28@gmail.com>
2023-06-25 23:54:44 +0700

    new sand

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -2,10 +2,12 @@ import '../CSS/form.css'
 import Header from '../component/Header'
 import Footer from '../component/Footer'
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const history = useNavigate();
 
   const inputUsername = (event) => {
     setUsername(event.target.value);
@@ -38,6 +40,29 @@ function LoginPage() {
     .catch(err => console.log(err));
   }  
 
+  const handleLogin1 = () => {
+    fetch('https://ep-tiny-limit-795119-pooler.us-east-1.postgres.vercel-storage.com/api/login', { 
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.success) {
+          console.log('Login berhasil');
+          alert(data.message);
+          history.push('/user-page', { username }); // Menggunakan react-router-dom untuk navigasi
+        } else {
+          console.log('Login gagal');
+          alert(data.message);
+        }
+      })
+      .catch(err => console.log(err));
+  };
+  
+
   return (
     <div className='mainContainer'>
         <Header />
@@ -49,7 +74,7 @@ function LoginPage() {
                     <input className='inputUsername' placeholder='Isi nama disini' value={username} onChange={inputUsername}/>
                     <h3>Password</h3>
                     <input type='password' className='inputPass'placeholder='Isi password disini' value={password} onChange={inputPassword}/>
-                    <button onClick={handleLogin}>Masuk akun</button>
+                    <button onClick={handleLogin1}>Masuk akun</button>
                     <div className='centerContainer'>
                       <h3>Belum punya akun?, <a href='/sign-in-page/'>daftar akun</a></h3>
                     </div>
